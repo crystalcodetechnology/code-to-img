efd2d93b272314d784dc3b2577f086b366c17b70
Rohid <rohidul209@gmail.com>
2022-12-17 11:56:20 +0600

    color remove

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,30 +8,30 @@ function IndexPage() {
     <>
       <Backgorund />
       <div className="flex w-fit mx-auto gap-4 my-8 mb-[250px]">
-        <aside className="w-[160px] h-[600px] bg-gray-100/10">
+        <aside className="w-[160px] h-[600px]">
           <div id="ezoic-pub-ad-placeholder-116"> </div>
         </aside>
         <div className="w-fit space-y-10">
-          <div className="w-[728px] h-[90px] bg-gray-100/10 mx-auto">
+          <div className="w-[728px] h-[90px] mx-auto">
             <div id="ezoic-pub-ad-placeholder-117"> </div>
           </div>
           <Preview />
-          <div className="w-[728px] h-[90px] bg-gray-100/10 mx-auto">
+          <div className="w-[728px] h-[90px] mx-auto">
             <div id="ezoic-pub-ad-placeholder-118"> </div>
           </div>
         </div>
         <aside className="space-y-4">
-          <div className="w-[300px] h-[250px] bg-gray-100/10">
+          <div className="w-[300px] h-[250px]">
             <div id="ezoic-pub-ad-placeholder-119"> </div>
           </div>
-          <div className="w-[300px] h-[250px] bg-gray-100/10">
+          <div className="w-[300px] h-[250px]">
             <div id="ezoic-pub-ad-placeholder-120"> </div>
           </div>
         </aside>
       </div>
       <ToolBar />
       <div className="w-full fixed bottom-0 bg-gray-900/50 backdrop-blur-xl z-30">
-        <div className="mx-auto h-[90px] w-[970px] bg-gray-100/10">
+        <div className="mx-auto h-[90px] w-[970px]">
           <div id="ezoic-pub-ad-placeholder-121"> </div>
         </div>
       </div>
